a314a048aa2d614d63c2f36cb926834ecbb41f20
leetech <orangesbdj@gmail.com>
2026-06-22 17:29:53 +0900

    chore: remove unused dead code

--- src/components/result/ImageBackground.tsx
+++
@@ -1,5 +0,0 @@
-import { View } from 'react-native';
-
-export default function ImageBackground() {
-  return <View />;
-}
--- src/navigation/MainTabNavigator.tsx
+++
@@ -1,3 +0,0 @@
-// MainTabNavigator → MainNavigator로 이름이 변경되었습니다.
-// 이 파일은 하위 호환성을 위한 re-export shim입니다. 직접 임포트하지 마세요.
-export { default } from './MainNavigator';
